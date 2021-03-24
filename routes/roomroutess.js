ba0af37d33e15b4a0fe86f0cd6e46698800d9792
Sudip Rai <raisudip05@gmail.com>
2021-03-24 18:30:20 +0545

    Code added for uploading photos in database

--- routes/roomroutess.js
+++ routes/roomroutess.js
@@ -30,4 +30,53 @@ router.post("/upload",authcheck.verifyUser, asyncHandler(async(req,res)=>{
 
 
 
+ router.put("/upload/:id",authcheck.verifyUser, asyncHandler(async(req,res,next)=>{
+    const room = await Room.findById(req.params.id);
+    const id=req.params.id
+    
+    if (!room) {
+      return next(new ErrorResponse(`No student found with ${req.params.id}`), 404);
+    }
+  
+  
+    if (!req.files) {
+      return next(new ErrorResponse(`Please upload a file`, 400));
+    }
+  
+    const file = req.files.file;
+  
+    if (file.size > process.env.MAX_FILE_UPLOAD) {
+      return next(
+        new ErrorResponse(
+          `Please upload an image less than ${process.env.MAX_FILE_UPLOAD}`,
+          400
+        )
+      );
+    }
+  
+    file.name = `photo_${room.id}${path.parse(file.name).ext}`;
+  
+    file.mv(`${process.env.FILE_UPLOAD_PATH}/${file.name}`,async (err)=> {
+      if (err) {
+        console.log(err);
+        return next(new ErrorResponse(`Problem with file upload`, 500));
+      }
+      await Room.findByIdAndUpdate(req.params.id, {
+        image: file.name,
+      });
+    });
+        
+    res.status(200).json({
+      success: true,
+      data: file.name,
+    });
+    
+   
+  
+ }));
+ 
+     
+
+
+
 module.exports=router
